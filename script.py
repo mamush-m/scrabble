e6176cc14970c3534fb419bdeb721c7aa2ed3f4e
mamush-m <svrmamush@gmail.com>
2023-05-26 23:32:58 -0400

    refactor

--- script.py
+++ script.py
@@ -4,42 +4,44 @@ def scrabbleScore(word):
     
     search_word = word.upper()
     total_score = 0
-    letter_scores = {
-        '1_point': {
+    letter_scores = [
+        {
             'point_value': 1,
             'letters': ['E', 'A', 'I', 'O', 'N', 'R', 'T', 'L', 'S', 'U']
         },
         
-        '2_point': {
+        {
             'point_value': 2,
             'letters': ['D', 'G']
         },
         
-        '3_point': {
+        {
             'point_value': 3,
             'letters': ['B', 'C', 'M', 'P']
         },
         
-        '4_point': {
+        {
             'point_value': 4,
             'letters': ['F', 'H', 'V', 'W', 'Y']
         },
         
-        '5_point': {
+        {
             'point_value': 5,
             'letters': ['K']
         },
         
-        '8_point': {
+        {
             'point_value': 8,
             'letters': ['J', 'X']
         },
         
-        '10_point': {
+        {
             'point_value': 10,
             'letters': ['Q', 'Z']
         }
-    }
+    ]
+    
+    
     
     
     
